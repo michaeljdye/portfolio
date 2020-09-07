a909b260a66a4c0b3c268be92e4f555288ef227f
Joey Dye <joey.m.dye@gmail.com>
2020-09-06 23:38:20 -0500

    feat: finish carousel functionality

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,37 +1,50 @@
+import { useState } from 'react'
 import styled from '@emotion/styled'
+import scenes from '../data/scenes.json'
 
 const Carousel = () => {
-  const scenes = [
-    {
-      id: 1,
-      title: 'My Reads',
-      description:
-        'The MyReads app provides a simple way to find new books and track your latest reads. Features include search books, add to bookshelf, track reads, and bulk move.',
-      stack: ['React', 'React Router', 'JS', 'CSS'],
-    },
-    {
-      id: 2,
-      title: 'Explore the Nations',
-      description:
-        'Explore The Nations features some of the top restaurants in The Nations. The app works with the Yelp and Google maps APIs to retrieve dynamic data about each location. In addition, Styled Components are used for applying custom styles.',
-      stack: ['React', 'Google Maps API', 'Yelp API', 'Styled Components'],
-    },
-    {
-      id: 3,
-      title: 'Space Explorer',
-      description:
-        'Explore stars, planets and other astronomical objects from the NASA database.',
-      stack: ['React', 'Node.js', 'Express', 'MongoDB', 'JWT'],
-    },
-  ]
+  const [activeScene, setActiveScene] = useState(0)
+
+  const handlePrevScene = () => {
+    if (activeScene <= 0) {
+      setActiveScene(scenes.length - 1)
+    } else {
+      setActiveScene(prevScene => prevScene - 1)
+    }
+  }
+
+  const handleNextScene = () => {
+    if (activeScene >= scenes.length - 1) {
+      setActiveScene(0)
+    } else {
+      setActiveScene(prevScene => prevScene + 1)
+    }
+  }
 
   return (
     <Slider>
-      <SiderItems>
-        <LeftArrow onClick={handlePrevSlide} />
-        <Slide />
-        <RightArrow onClick={handleNextSlide} />
-      </SiderItems>
+      <SliderItems>
+        <LeftArrow onClick={handlePrevScene}>Left</LeftArrow>
+        <ul>
+          {scenes
+            .filter((scene, index) => index === activeScene)
+            .map(({ id, title, description, stack, demo, source }) => (
+              <Slide key={id}>
+                <h1>{title}</h1>
+                <p>{description}</p>
+                <Stack>
+                  {stack.map(stackItem => (
+                    <li key={stackItem}>{stackItem}</li>
+                  ))}
+                </Stack>
+                <a href={demo}>View Demo</a>
+                <a href={source}>View Source</a>
+              </Slide>
+            ))}
+        </ul>
+
+        <RightArrow onClick={handleNextScene}>Right</RightArrow>
+      </SliderItems>
     </Slider>
   )
 }
@@ -42,8 +55,22 @@ const SliderItems = styled.div``
 
 const Slide = styled.div``
 
-const LeftArrow = styled.div``
+const LeftArrow = styled.button``
 
-const RightArrow = styled.div``
+const RightArrow = styled.button``
+
+const Stack = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-weight: 700;
+
+  li + li {
+    border-left: 1px solid black;
+    margin-left: 10px;
+    padding-left: 10px;
+  }
+`
 
 export default Carousel
