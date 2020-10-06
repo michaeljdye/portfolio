ab43bd6b22ee1bbd8c8c569ad1fbe8947d5fb6b5
Joey Dye <joey.m.dye@gmail.com>
2020-10-06 17:02:54 -0500

    feat: start work on mobile menu

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { Link } from 'next/link'
+import { below } from '../../utils'
 
 const Nav = () => (
   <Navigation>
@@ -10,7 +11,7 @@ const Nav = () => (
       <NavItem>
         <NavLink href='#projects'>Apps</NavLink>
       </NavItem>
-      <NavItem>
+      <NavItem className='logo'>
         <Logo src='/images/joey-dye-logo.png' alt='Joey Dye' />
       </NavItem>
       <NavItem>
@@ -20,6 +21,21 @@ const Nav = () => (
         <NavLink href='#contact'>Contact</NavLink>
       </NavItem>
     </NavItems>
+
+    <MobileNavItems>
+      <NavItem>
+        <NavLink href='#about'>about</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href='#projects'>Apps</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href='#websites'>Websites</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href='#contact'>Contact</NavLink>
+      </NavItem>
+    </MobileNavItems>
   </Navigation>
 )
 
@@ -38,15 +54,28 @@ const NavItems = styled.ul`
   padding: 0;
 `
 
+const MobileNavItems = styled.div`
+  display: none;
+`
+
 const NavItem = styled.li`
   text-transform: uppercase;
   font-size: 1.3rem;
   font-weight: 500;
   letter-spacing: 2.3px;
 
+  ${below.phone`
+    &:not(.logo) {
+      display: none;
+    }
+  `}
+
   & + li {
     margin-left: 60px;
-  }
+
+    ${below.phone`
+      margin-left: 0;
+    `}
 `
 
 const NavLink = styled.a`
