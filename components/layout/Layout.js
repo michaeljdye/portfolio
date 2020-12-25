e1ccdee04d05195f43ac6ac147def1978148bf38
Joey Dye <joey.m.dye@gmail.com>
2020-12-25 00:22:30 -0600

    feat: create theme toggle

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import Head from 'next/head'
 import { css, Global } from '@emotion/core'
 import emotionNormalize from 'emotion-normalize'
@@ -5,98 +6,113 @@ import { below } from '../../utils/breakpoints'
 import Hero from '../sections/Hero'
 import Footer from './Footer'
 
-const Layout = ({ children, pageTitle, siteTitle, heading, subheading }) => (
-  <>
-    <Head>
-      <title>
-        {pageTitle} | {siteTitle}
-      </title>
-      <meta
-        name='description'
-        content='I am a fullstack developer from Nashville, TN.'
-      />
-    </Head>
-    <Global
-      styles={css`
-        :root {
-          --colorPrimary: #04f5ca;
-          --colorSecondary: #05caa7;
-          --colorDark: #111111;
-          --colorGray: #262626;
-          --colorMediumGray: #eee;
-          --colorLightGray: #fefefe;
-          --colorWhite: #ffffff;
-          --colorTextLight: #bbb;
-          --defaultPadding: 80px;
+const Layout = ({ children, pageTitle, siteTitle, heading, subheading }) => {
+  const containerRef = useRef()
 
-          ${below.phone`
+  const handleThemeChange = () => {
+    containerRef.current.classList.toggle('dark')
+  }
+
+  return (
+    <>
+      <Head>
+        <title>
+          {pageTitle} | {siteTitle}
+        </title>
+        <meta
+          name='description'
+          content='I am a fullstack developer from Nashville, TN.'
+        />
+      </Head>
+      <Global
+        styles={css`
+          .theme {
+            --colorPrimary: #04f5ca;
+            --colorSecondary: #05caa7;
+            --colorDark: #111111;
+            --colorGray: #262626;
+            --colorMediumGray: #eee;
+            --colorLightGray: #fefefe;
+            --colorWhite: #ffffff;
+            --colorTextLight: #bbb;
+            --defaultPadding: 80px;
+
+            ${below.phone`
             --defaultPadding: 60px;
           `}
-        }
+          }
 
-        ${emotionNormalize}
+          .theme.dark {
+            --colorPrimary: #181818;
+          }
 
-        html {
-          box-sizing: border-box;
-          scroll-behavior: smooth;
-        }
+          ${emotionNormalize}
 
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          background: white;
-          min-height: 100%;
-          font-family: 'Montserrat', sans-serif;
-        }
+          html {
+            box-sizing: border-box;
+            scroll-behavior: smooth;
+          }
 
-        *,
-        *:before,
-        *:after {
-          box-sizing: inherit;
-        }
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            background: white;
+            min-height: 100%;
+            font-family: 'Montserrat', sans-serif;
+          }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5 {
-          font-family: 'Roboto Slab', serif;
-          font-weight: 400;
-          line-height: 1.15;
-        }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
 
-        h1 {
-          margin: 0;
-          text-transform: uppercase;
-          font-size: max(7vw, 2rem);
-          font-weight: 700;
-        }
+          h1,
+          h2,
+          h3,
+          h4,
+          h5 {
+            font-family: 'Roboto Slab', serif;
+            font-weight: 400;
+            line-height: 1.15;
+          }
 
-        h2 {
-          margin: 0;
-          text-transform: uppercase;
-          font-weight: 700;
-          font-size: max(4vw, 2.5rem);
-        }
+          h1 {
+            margin: 0;
+            text-transform: uppercase;
+            font-size: max(7vw, 2rem);
+            font-weight: 700;
+          }
 
-        h3 {
-          font-size: max(2vw, 1.25rem);
-          font-weight: 700;
-          text-transform: uppercase;
-        }
+          h2 {
+            margin: 0;
+            text-transform: uppercase;
+            font-weight: 700;
+            font-size: max(4vw, 2.5rem);
+          }
 
-        p,
-        li {
-          font-size: max(1vw, 1.125rem);
-          line-height: 1.4;
-        }
-      `}
-    />
-    <Hero heading={heading} subheading={subheading} />
-    {children}
-    <Footer />
-  </>
-)
+          h3 {
+            font-size: max(2vw, 1.25rem);
+            font-weight: 700;
+            text-transform: uppercase;
+          }
+
+          p,
+          li {
+            font-size: max(1vw, 1.125rem);
+            line-height: 1.4;
+          }
+        `}
+      />
+      <div ref={containerRef} className='theme'>
+        <Hero heading={heading} subheading={subheading} />
+        <button onClick={handleThemeChange}>Change theme</button>
+        {children}
+        <Footer />
+      </div>
+    </>
+  )
+}
 
 export default Layout
