4d261bb65352c1b78d196edd49b799e4e6babf64
Michael Dye <32441293+michaeljdye@users.noreply.github.com>
2022-02-05 01:28:43 -0600

    Add word I

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import Layout from '../components/layout/Layout'
-import Hero from '../components/sections/Hero'
-import Bio from '../components/sections/Bio'
-import Apps from '../components/sections/Apps'
-import Sites from '../components/sections/Sites'
-import Skills from '../components/sections/Skills'
-import CTA from '../components/sections/CTA'
-import Footer from '../components/layout/Footer'
-import Contact from '../components/forms/Contact'
+import Layout from "../components/layout/Layout";
+import Hero from "../components/sections/Hero";
+import Bio from "../components/sections/Bio";
+import Apps from "../components/sections/Apps";
+import Sites from "../components/sections/Sites";
+import Skills from "../components/sections/Skills";
+import CTA from "../components/sections/CTA";
+import Footer from "../components/layout/Footer";
+import Contact from "../components/forms/Contact";
 
 const Home = () => (
   <Layout
-    pageTitle='Michael Dye'
-    siteTitle='Full Stack Developer'
-    heading='Build. Ship. Repeat'
-    subheading=' am a growth-driven developer dedicated to building a better web.'
+    pageTitle="Michael Dye"
+    siteTitle="Full Stack Developer"
+    heading="Build. Ship. Repeat"
+    subheading="I am a growth-driven developer dedicated to building a better web."
   >
     <Bio />
     <Apps />
@@ -22,6 +22,6 @@ const Home = () => (
     <Skills />
     <Contact />
   </Layout>
-)
+);
 
-export default Home
+export default Home;
