197ccf10d8f828ec56ca3225d6f2194051c2a71c
Michael Dye <hello@michaeldye.dev>
2023-06-06 14:38:48 -0500

    fix: Remove extra skills

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Layout from "../components/layout/Layout";
-import Hero from "../components/sections/Hero";
 import Bio from "../components/sections/Bio";
 import Apps from "../components/sections/Apps";
 import Sites from "../components/sections/Sites";
 import Skills from "../components/sections/Skills";
 import CTA from "../components/sections/CTA";
-import Footer from "../components/layout/Footer";
 import Contact from "../components/forms/Contact";
 
 const Home = () => (
